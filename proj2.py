7708755052530176cf5f591ef127d00f7c62930c
Sankarshan Araujo <sa343@uakron.edu>
2021-03-06 16:37:30 -0500

    convert data to csv

--- proj2.py
+++ proj2.py
@@ -5,5 +5,13 @@
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
+data = pd.read_csv("data.csv")
+dataset = pd.DataFrame(data = data)
+
+plt.plot(dataset["Yards"], dataset["Goal Success"], 'ro')
+plt.ylabel('Goal Success')
+plt.xlabel('Yards')
+plt.show()
 
