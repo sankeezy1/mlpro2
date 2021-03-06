8e77c295b246f8456d01e6ac7aec640f3cde560d
Sankarshan Araujo <sa343@uakron.edu>
2021-03-06 17:50:22 -0500

    Basic code

--- proj2.py
+++ proj2.py
@@ -10,7 +10,29 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
 data = pd.read_csv("data.csv")
-dataset = pd.DataFrame(data = data)
-print(X)
+df = pd.DataFrame(data = data)
+
+X = df[["Yards"]]
+y = df["Success"]
+plt.plot(X, y, 'ro')
+plt.ylabel('success')
+plt.xlabel('yards')
+# plt.show()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+lr = LogisticRegression()
+lr.fit(X_train, y_train)
+
+print("lr.coef_: {}".format(lr.coef_))
+print("lr.intercept_: {}".format(lr.intercept_))
+
+print("Training set score: {:.2f}".format(lr.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(lr.score(X_test, y_test)))
+
+yards = 52
+pass_prediction = lr.predict([[yards]])
+pass_probability = lr.predict_proba([[yards]])
+print("pass: {}".format(pass_prediction[0]))
+print("fail/pass probability: {}".format(pass_probability[0]))
 
 
